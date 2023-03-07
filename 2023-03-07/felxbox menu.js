73f8f50674226b9fc54c06c43fdc4662d4f75b39
EvelinHollik2 <hollikevi@gmail.com>
2023-03-07 10:49:50 +0100

    hiuo

---
+++ 2023-03-07/felxbox menu.js
@@ -0,0 +1,8 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+<script>
+    $(function () {
+        $('.toggleNav').on('click', function () {
+            $('.flex-nav ul').toggleClass('open');
+        });
+    });
+</script>
